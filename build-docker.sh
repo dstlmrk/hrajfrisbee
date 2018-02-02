34259a01f947b0fb66028fb05a0fcd51ff3de9d4
Honza Novak <kacerr.cz@gmail.com>
2018-02-02 20:14:34 +0100

    Add Dockerfile and build script

---
+++ build-docker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+image_name=hrajfrisbee.cz
+
+docker build -t $image_name:`git rev-parse HEAD` . --no-cache --build-arg GIT_COMMIT_HASH=`git rev-parse HEAD`
+
+final_tag="registry.simplifate.zlutazimnice.cz/$image_name:`git rev-parse HEAD`"
+docker tag $image_name:`git rev-parse HEAD` $final_tag
+docker push $final_tag
+echo "Container has been pushed as: ${final_tag}"
