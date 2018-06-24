b7f9ecd8e4feafd799558b5603f7568f6487eebf
Honza Novak <kacerr.cz@gmail.com>
2018-06-24 12:42:14 +0200

    pipeline: debug print $CI_BUILD_TOKEN

--- build-docker.sh
+++ build-docker.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+set -x
 image_name=hrajfrisbee.cz
 
 docker build -t $image_name:`git rev-parse HEAD` . --no-cache --build-arg GIT_COMMIT_HASH=`git rev-parse HEAD`
 
-final_tag="registry.simplifate.zlutazimnice.cz/$image_name:`git rev-parse HEAD`"
+final_tag="registry.gitlab.zlutazimnice.cz/$image_name:`git rev-parse HEAD`"
 docker tag $image_name:`git rev-parse HEAD` $final_tag
 docker push $final_tag
 echo "Container has been pushed as: ${final_tag}"
